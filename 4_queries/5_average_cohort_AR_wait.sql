badac12148c06fd4719a24e5819b5b374f96b80b
Kennie <Kenneth Chan>
2022-12-13 15:03:23 -0500

    add 4_queries

---
+++ 4_queries/5_average_cohort_AR_wait.sql
@@ -0,0 +1,6 @@
+SELECT cohorts.name, avg(assistance_requests.completed_at - assistance_requests.started_at) as average_assistance_time
+FROM cohorts
+JOIN students ON cohorts.id = cohort_id
+JOIN assistance_requests ON students.id = student_id
+GROUP BY cohorts.name
+ORDER BY average_assistance_time;
